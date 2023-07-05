092f149e472951d04cad4d5b1e261455cc4c68b1
Jhony Hernandez <jhonyh09@hotmail.com>
2023-07-05 16:46:40 -0400

    Configuracion variables de entorno y libreria dotenv

--- controllers/whatsapp.controller.js
+++ controllers/whatsapp.controller.js
@@ -1,11 +1,9 @@
-import {token} from '../.env'
-
 export const enviarSolicitudWS = async (req, res) => {
   try {
     const url = 'https://graph.facebook.com/v17.0/101112756371877/messages/';
     const headers = {
       'Content-Type': 'application/json',
-      'Authorization': token
+      'Authorization': 'Bearer EAAEEDinZB4nEBAFNKgl8mtGHYcDfRysQd6eiEZAnf5DP4SB5bX7jZCiUkEjCWs2HCnuZCCQ4iijgMvuGuCMigWkuYCj81sCNSbID3P3aNDMnpPzr1Jeta5l8Lnr4tSCG0R1NpDFKIzDV4N7T5BObx9Dv0ZC1uwf30Pdo5fXqUOwPn4QSwVRFu'
     };
     const body = JSON.stringify({
       "messaging_product": "whatsapp",
--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 import app from './app.js';
 import { connectDb } from './db.js';
 
+
+//Importación de las variables de entorno
+import dotenv from 'dotenv';
+dotenv.config();
+let PORT = process.env.PORT || 3000;
+let url= process.env.Urldb;
+let db = process.env.db;
+
+
+
 // Inicializacion de la conection a la DB
-connectDb('127.0.0.1', 'donremolodb');
+connectDb(url, db);
 
 // Method para iniciar la escucha de express RUN
-let PORT = 4000;
 app.listen(PORT);
 console.log('Server is Runing in Port: '+ PORT);
